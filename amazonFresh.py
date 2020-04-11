000560b73d9e7d89e2f8cced9b5cdf6c9dd15dc1
Prateek <ptsingh1000@yahoo.com>
2020-04-11 19:35:57 -0400

    Update amazonFresh.py
    corner case handling of timed re-direction to cart checkout page.

--- amazonFresh.py
+++ amazonFresh.py
@@ -12,10 +12,8 @@ from twilio.rest import Client
 #Please Read: I am yet to get a succesful slot on Amazon Fresh so I do not know exactly how
 # the page looks like when slots are available so I am currently checking only based
 # on what I see when there are no slots available.
-# This might not be a 100% through check as atleast with PrimeNow/Wholefoods I have noticed that
-# sometimes the page stops displaying this text after many refresh cycles and that is then 
-# assumed to be a availability signal and a text is send.
-# This is different thant the amazonPrimeNow.py that actually checks for a positive
+# This might not be a 100% through check.
+# This is different than the amazonPrimeNow.py that actually checks for a positive
 # match on text that appears only when a window is available (at the time of this writing)
 #############################################################################################
 
@@ -28,32 +26,30 @@ class AutomateGroceryDelivery:
         self.username = args.user
         self.password = args.password
 
-        self.my_mobile = "your cell number here"
-        self.twilio_num = "registered twilio number here"
+        self.my_mobile = "your cell_number"
+        self.twilio_num = " registered twilio_number"
         self.text_content = "Slots Available on Amazon Fresh. Order now!"
         self.long_pause = 20
         self.short_pause = 2
         
         # THis is the right way to source sid and token. Put these an env file and source it in python
-        # You *can* put them directly here as well but that is **INSECURE**
+        # You *can* put them directly here as well but that is **INSCEURE**
         self.account_sid = os.environ['TWILIO_ACCOUNT_SID']
         self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
-        self.amazon_fresh_url = 'amazon fresh sign in page url here'
-        self.driver = webdriver.Chrome('optional path_to_driver')  # Optional argument, if not specified will search path.
+        self.amazon_fresh_url = 'amazon fresh sign in url'
+        self.driver = webdriver.Chrome('location of your selenium webdriver')  # Optional argument, if not specified will search path.
         self.driver.maximize_window()
 
     def send_sms(self):
         # Your Account Sid and Auth Token from twilio.com/console
         print("sending sms...")
         client = Client(self.account_sid, self.auth_token)
-
         client.messages.create(
             to=self.my_mobile, 
             from_=self.twilio_num,
             body=self.text_content)
 
     def login(self, redirect = True):
-
         # login is called from two places:
         # 1.) initial login
         # 2.) when password is asked again for sign in
@@ -64,12 +60,12 @@ class AutomateGroceryDelivery:
             user_name.clear()
             user_name.send_keys(self.username)
             time.sleep(self.short_pause) # add delay to stop the captcha kickin
+            
+            continue_btn = self.driver.find_element_by_id('continue')
+            continue_btn.click()
+            time.sleep(self.short_pause)
         except:
             pass # sometimes only password is asked so expected that username is not present
-
-        continue_btn = self.driver.find_element_by_id('continue')
-        continue_btn.click()
-        time.sleep(self.short_pause)
 
         password = self.driver.find_element_by_id('ap_password')
         password.clear()
@@ -81,7 +77,7 @@ class AutomateGroceryDelivery:
         try:
             # if we find email entry again then this means that we got captcha
             self.driver.find_element_by_id('ap_email')
-            time.sleep(self.long_pause) # allow time for user to enter captcha
+            time.sleep(self.long_pause) # allow time to enter captcha
             self.login(False)
         except: # handle OTP
             # allow time for optional OTP authentication
@@ -97,12 +93,12 @@ class AutomateGroceryDelivery:
         # go to fresh checkout page
         checkout_btn = self.driver.find_element_by_xpath('//input[starts-with(@name,"proceedToALMCheckout")]')
         checkout_btn.click()
-
         time.sleep(self.short_pause)
-        
+
         # Before you checkout page
         continue_btn = self.driver.find_element_by_xpath('//a[@name="proceedToCheckout"]')
         continue_btn.click()
+        time.sleep(self.short_pause)
 
         # see if password is asked again
         # if so then enter it
@@ -156,9 +152,8 @@ class AutomateGroceryDelivery:
 
                 # unable to find any elements that indicate that slots are not available
                 # This is not a 100% solid assumption! But will have to do for now
-                slot_available = True      
+                slot_available = True
                 
-
         # Delivery slot available! Send sms
         self.send_sms()     
 
@@ -166,4 +161,3 @@ class AutomateGroceryDelivery:
 a = AutomateGroceryDelivery()
 a.login()
 a.goCartCheckoutReadyAndNotify()
-
